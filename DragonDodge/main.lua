967177fa8b6d2cea23f32520acc72544e2bad546
viserya <ibtingzon@yahoo.com.ph>
2014-06-09 08:16:22 -0700

    Remove

--- DragonDodge/main.lua
+++
@@ -1,7 +0,0 @@
-display.setStatusBar( display.HiddenStatusBar )
-
-local storyboard = require("storyboard")
-
-storyboard.purgeOnSceneChange = true
-storyboard.gotoScene("menuScreen")
---local mainGame = require("mainGame")
